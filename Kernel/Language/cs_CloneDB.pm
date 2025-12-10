58cf410339b997eae6d5b31d3b7b4cd499760e8d
Balázs Úr <balazs@urbalazs.hu>
2025-12-10 23:21:00 +0100

    Regenerated message catalog, fixed Czech language file.

--- Kernel/Language/cs_CloneDB.pm
+++ Kernel/Language/cs_CloneDB.pm
@@ -16,31 +16,14 @@ sub Data {
     my $Self = shift;
 
     # SysConfig
-    $Self->{Translation}->{'Customizable system name. If empty, config item \'Home\' is being used for display.'} =
+    $Self->{Translation}->{'List of tables should be skipped, perhaps internal DB tables. Please use lowercase.'} =
         '';
-    $Self->{Translation}->{'Defines if Fred is visible or hidden.'} = '';
-    $Self->{Translation}->{'Defines the height of the Fred console in pixel.'} = '';
-    $Self->{Translation}->{'Defines the log path for Freds logs.'} = '';
-    $Self->{Translation}->{'Defines the opacity of the Fred console, e.g. 0.9 for 10% transparency.'} =
+    $Self->{Translation}->{'Log file for replacement of malformed UTF-8 data values.'} = '';
+    $Self->{Translation}->{'Settings for connecting with the target database.'} = '';
+    $Self->{Translation}->{'Specifies which columns should be checked for valid UTF-8 source data.'} =
         '';
-    $Self->{Translation}->{'Defines the width of the Fred console in percent.'} = '';
-    $Self->{Translation}->{'Frontend module registration for the Fred object in the admin area.'} =
+    $Self->{Translation}->{'This setting specifies which table columns contain blob data as these need special treatment.'} =
         '';
-    $Self->{Translation}->{'Subscribe the output filter at the system.'} = '';
-    $Self->{Translation}->{'This option defines the background color for the sytem name box.'} =
-        '';
-    $Self->{Translation}->{'Turn on if you want an real-time HTML-checker.'} = '';
-    $Self->{Translation}->{'Turn on if you want an real-time SQL statement log function.'} =
-        '';
-    $Self->{Translation}->{'Turn on if you want the translation debugger.'} = '';
-    $Self->{Translation}->{'Turn on if you want to see the STDERRLog.'} = '';
-    $Self->{Translation}->{'Turn on if you want to see the data in the environment of the layout object.'} =
-        '';
-    $Self->{Translation}->{'Turn on if you want to see the data in the server side session object.'} =
-        '';
-    $Self->{Translation}->{'Turn on if you want to use the Fred console.'} = '';
-    $Self->{Translation}->{'Turn on if you want to use the config log mechanism.'} = '';
-    $Self->{Translation}->{'Turn on if you want to use the config switch mechanism.'} = '';
 
 
     push @{ $Self->{JavaScriptStrings} //= [] }, (
